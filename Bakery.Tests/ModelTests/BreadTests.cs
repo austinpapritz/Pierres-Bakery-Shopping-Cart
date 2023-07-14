36bcdefc86cc5efa067231a81b8a47715f2cd087
Austin Papritz <austin@papritz.dev>
2023-07-14 09:39:33 -0700

    BreadPriceCalc return price test pass

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,8 +9,20 @@ public class BreadTests
     public void BreadConstructor_CreateInstanceOfBread()
     {
         // Arrange
-        Bread newBread= new Bread(1);
+        Bread newBread = new Bread(1);
         // Assert
         Assert.AreEqual(typeof(Bread),newBread.GetType());
+    }
+
+    [TestMethod]
+    public void BreadPriceCalc_ReturnProductOfCountAndPrice_Int()
+    {
+        // Arrange
+        int expectedPrice = 25;
+        Bread newBread = new Bread(5);
+        // Act
+        int testPrice = newBread.BreadPriceCalc();
+        // Assert
+        Assert.AreEqual(expectedPrice, testPrice);
     }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -3,9 +3,15 @@ namespace Bakery.Models;
 public class Bread
 {
     public int Count { get; set;}
+    private int _price = 5;
 
     public Bread(int count)
     {
         Count = count;
+    }
+
+    public int BreadPriceCalc()
+    {
+        return Count * _price;
     }
 }
