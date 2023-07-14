1c3c97ed9112f9e56b4c44e28441d0b86081026c
Austin Papritz <austin@papritz.dev>
2023-07-14 13:48:16 -0700

    basic ordering and totalling UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bakery.Models;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main() 
+    {
+      // Welcome.
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to Pappy's Bakery!");
+      Console.WriteLine("A loaf of bread is $5, a pastry is $2!");
+      Console.WriteLine("Loaves of bread are buy 2 get 1 free!");
+      Console.WriteLine("Pastries are buy 3 get 1 free!");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+
+      // Take bread order.
+      Console.WriteLine("How many loaves of bread do you want?");
+      int numOfLoaves = int.Parse(Console.ReadLine());
+      // Calculate bread total.
+      Bread breadOrder = new Bread(numOfLoaves);
+      int breadTotal = breadOrder.BreadPriceCalc();
+
+      // Take pastry order.
+      Console.WriteLine("How many pastries do you want?");
+      int numOfPastries = int.Parse(Console.ReadLine());
+      // Calculate pastry total.
+      Pastry pastryOrder = new Pastry(numOfPastries);
+      int pastryTotal = pastryOrder.PastryPriceCalc();
+
+      // Calculate and tell user the order total.
+      Console.WriteLine("Here are your totals");
+      Console.WriteLine($"Bread ({numOfLoaves}) = ${breadTotal}");
+      Console.WriteLine($"Pastry ({numOfPastries}) = ${pastryTotal}");
+      Console.WriteLine($"Your total = ${breadTotal + pastryTotal}");
+    }
+}
