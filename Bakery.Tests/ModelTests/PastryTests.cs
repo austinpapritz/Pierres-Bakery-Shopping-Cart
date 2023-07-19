795e77b6bb60136dd0c15690db7d41b222e85bbb
Austin Papritz <austin@papritz.dev>
2023-07-19 14:33:28 -0700

    full price pastry calc test pass

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -14,58 +14,24 @@ public class PastryTests
         Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
-        [TestMethod]
-    public void PastryPriceCalc_ReturnProductOfCountAndPrice_Int()
-    {
-        // Arrange
-        int expectedPrice = 2;
-        Pastry newPastry = new Pastry(1);
-        // Act
-        int testPrice = newPastry.PastryPriceCalc();
-        // Assert
-        Assert.AreEqual(expectedPrice, testPrice);
-    }
-
-    [Ignore]
-    [TestMethod]
-    public void PastryPriceCalc_CalculateDiscount_Int()
-    {
-        // Arrange
-        int noDiscount = 0;
-        int discount = 2;
-        Pastry onePastry = new Pastry(1);
-        Pastry twoPastry = new Pastry(2);
-        Pastry threePastry = new Pastry(3);
-        Pastry fourPastry = new Pastry(4);
-        // Act
-        int discountOnePastry = onePastry.PastryPriceCalc(); 
-        int discountTwoPastry = twoPastry.PastryPriceCalc(); 
-        int discountThreePastry = threePastry.PastryPriceCalc();
-        int discountFourPastry = fourPastry.PastryPriceCalc();
-        // Assert
-        Assert.AreEqual(discountOnePastry, noDiscount);
-        Assert.AreEqual(discountTwoPastry, noDiscount);
-        Assert.AreEqual(discountThreePastry, noDiscount);
-        Assert.AreEqual(discountFourPastry, discount);
-    }
-
      [TestMethod]
-    public void PastryPriceCalc_CalculatePriceMinusDiscount_Int()
+    public void PastryPriceCalc_CalculateFullPricePastry_Int()
     {
         // Arrange
-        int expectedPriceSix = 10;
-        int expectedPriceSevenAndEight = 12;
-        Pastry sixPastry = new Pastry(6);
+        int expectedPriceTwo = 4;
+        int expectedPriceFive = 8;
+        int expectedPriceSeven = 12;
+        Pastry twoPastry = new Pastry(2);
+        Pastry fivePastry = new Pastry(5);
         Pastry sevenPastry = new Pastry(7);
-        Pastry eightPastry = new Pastry(8);
         // Act
-        int sixPastryTotal = sixPastry.PastryPriceCalc(); 
-        int sevenPastryTotal = sevenPastry.PastryPriceCalc(); 
-        int eightPastryTotal = eightPastry.PastryPriceCalc();
+        int twoPastryTotal = twoPastry.PastryPriceCalc(); 
+        int fivePastryTotal = fivePastry.PastryPriceCalc(); 
+        int sevenPastryTotal = sevenPastry.PastryPriceCalc();
         // Assert
-        Assert.AreEqual(sixPastryTotal, expectedPriceSix);
-        Assert.AreEqual(sevenPastryTotal, expectedPriceSevenAndEight);
-        Assert.AreEqual(eightPastryTotal, expectedPriceSevenAndEight);
+        Assert.AreEqual(twoPastryTotal, expectedPriceTwo);
+        Assert.AreEqual(fivePastryTotal, expectedPriceFive);
+        Assert.AreEqual(sevenPastryTotal, expectedPriceSeven);
     }
 
 }
